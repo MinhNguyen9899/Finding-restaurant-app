17fa78d579bc05bdd610f09dedf1a8b5ce122c8a
MinhNguyen9899 <ntminhit98@gmail.com>
2026-06-07 15:27:09 +0700

    fix database and requirements

--- backend/database.py
+++ backend/database.py
@@ -1,16 +1,21 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    )
 SessionLocal = sessionmaker(
      autocommit=False,
     autoflush=False,
     bind=engine)
+
 Base = declarative_base()
 
 def get_db():
